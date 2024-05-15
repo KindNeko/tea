cebb04b7b50e9f40edb3af656ca7914c27868dec
Maxim Pantelidi <maksim.pantelidi@yandex.ru>
2024-05-15 13:14:13 +0300

    полный сайт

---
+++ js/script.js
@@ -0,0 +1,27 @@
+const menu = {
+  overlay: document.querySelector(".menu-overlay"),
+  list: document.querySelector(".menu__list"),
+  listFooter: document.querySelector(".footer__products"),
+
+  toggleMenu(selector) {
+    document.querySelector(selector).addEventListener("click", () => {
+      this.list.classList.toggle("menu__list--open");
+      this.overlay.classList.toggle("menu-overlay--show");
+    });
+  },
+
+  smoothBehavior(list, link) {
+    list.addEventListener("click", (e) => {
+      e.preventDefault();
+      if (e.target.classList.contains(link)) {
+        const href = e.target.getAttribute("href");
+        document.querySelector(href).scrollIntoView({ behavior: "smooth" });
+      }
+    });
+  },
+};
+
+menu.toggleMenu(".menu__btn");
+menu.toggleMenu(".menu__close-btn");
+menu.smoothBehavior(menu.list, "menu__link");
+menu.smoothBehavior(menu.listFooter, "footer__products-link");
